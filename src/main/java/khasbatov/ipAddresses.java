af4acdca560e04b7df634106854a745f00320e64
sonikjunior <sonikjunior@rambler.ru>
2017-01-11 12:28:04 +0300

    Add methods for transfer int array to long and lond to string

--- src/main/java/khasbatov/ipAddresses.java
+++ src/main/java/khasbatov/ipAddresses.java
@@ -6,13 +6,7 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * Created by ruslan on 10.01.17.
- */
 public class ipAddresses {
-
-    final int MIN_IP_VALUE = 0;
-    final int MAX_IP_VALUE = 255;
 
     public static void main(String[] args) throws IOException {
 
@@ -21,40 +15,55 @@ public class ipAddresses {
         String first_ip = reader.readLine();
         String last_ip = reader.readLine();
 
-        int [] first_ip_arr = stringParser(first_ip);
-        int [] last_ip_arr = stringParser(last_ip);
+        int[] first_ip_arr = stringParser(first_ip);
+        int[] last_ip_arr = stringParser(last_ip);
 
+        long first = ipArrToLong(first_ip_arr);
+        long last = ipArrToLong(last_ip_arr);
+
+        while (first<last-1) {
+            first++;
+            System.out.println(longToString(first));
+        }
     }
 
+    public static long ipArrToLong(int [] ip_arr) {
+
+        return (Long.valueOf(ip_arr[0]) << 24) +
+                (Long.valueOf(ip_arr[1]) << 16) +
+                (Long.valueOf(ip_arr[2]) << 8) +
+                (Long.valueOf(ip_arr[3]));
+    }
+
+    public static String longToString(long val) {
+
+        StringBuffer buffer = new StringBuffer();
+        buffer.append((val & 0xFF000000) >> 24).append('.');
+        buffer.append((val & 0x00FF0000) >> 16).append('.');
+        buffer.append((val & 0x0000FF00) >> 8).append('.');
+        buffer.append(val & 0x000000FF);
+        return buffer.toString();
+    }
 
     public static int[] stringParser(String string){
 
         List<String> ip_list = new ArrayList<String>();
         String ip_block="";
         for(int i=0; i<string.length(); i++){
-
             if(!string.substring(i,i+1).equals(".")){
                 ip_block = ip_block.concat(string.substring(i,i+1));
             } else {
                 ip_list.add(ip_block);
                 ip_block = "";
             }
-
             if(i == string.length()-1){
                 ip_list.add(ip_block);
             }
         }
-        System.out.println(ip_list);
-
         int [] first_ip = new int[4];
         for (int i = 0; i < 4; i++) {
-            first_ip[i]=Integer.parseInt(ip_list.get(i));
+            first_ip[i] = Integer.parseInt(ip_list.get(i));
         }
-
         return first_ip;
-
     }
-
 }
-
-
